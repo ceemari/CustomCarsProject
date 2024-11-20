bb39547c760286055e9c3f1e640c3ebaf3b2e5c6
Cee Mari <37902145-chamarimclean@users.noreply.replit.com>
2024-11-20 23:14:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# Print title of form
+print("========================================")
+print("Welcome to the UMBC Car Customization Form")
+print("========================================")
+
+# Print instructions for the form
+print("Please enter the corresponding letter of your model")
+print("1. What model of car are you ordering?")
+
+# Print users' choices of models
+print(1)
+
+# Prompt user for the corresponding letter of their model
+a = input("Lightning Speedster ")
+b = input("Eco Leaf ")
+c = input("Harp Chord")
+d = input("Chevron Jolt")
+
