8d6e7291fa43e2843e949d0cad3f37c5dd920073
ceemari <chamari.mclean@gmail.com>
2024-11-21 00:35:40 +0000

    user model choices lines 10 and 11

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ print("Please enter the corresponding letter of your model")
 print("1. What model of car are you ordering?")
 
 # Print users' choices of models
-print(1)
+print("a. Lightning Speedster + b. Eco Leaf + c. Harp Chord + d. Chevron Jolt")
 
 # Prompt user for the corresponding letter of their model
 a = input("Lightning Speedster ")
